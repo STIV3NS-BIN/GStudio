4b37e2e76f7ee1b593560486db73932cbc0bbccf
Jacek <jacekbizub@gmail.com>
2018-09-19 16:43:12 +0200

    Changed .show-more to anchor and compressed CSS

--- res/js/scrollbtns.js
+++ res/js/scrollbtns.js
@@ -3,7 +3,8 @@
 
 	let btns = document.querySelectorAll('.scroll-btn');
 	for (let btn of btns) {
-		btn.addEventListener('click', function() {
+		btn.addEventListener('click', function(e) {
+			e.preventDefault();
 			document.querySelector(btn.dataset.target).scrollIntoView({ 
 			  behavior: 'smooth' 
 			});
