904a562c7c270055f5bdb77b9fe49173c1e533fa
Jacek <jacekbizub@gmail.com>
2018-09-24 22:57:08 +0200

    Replaced .gallery with .galleries

--- res/js/cssloader.js
+++ res/js/cssloader.js
@@ -1,8 +1,7 @@
 (function(){
     let head  = document.getElementsByTagName('head')[0];
     let link;
-    let srcs = ['https://fonts.googleapis.com/css?family=Oswald:200,400,500,700&amp;subset=latin-ext',
-                'https://cdn.plyr.io/3.4.3/plyr.css'];
+    let srcs = ['https://fonts.googleapis.com/css?family=Oswald:200,400,500,700&amp;subset=latin-ext'];
 
     for (src of srcs) {
         link  = document.createElement('link');
