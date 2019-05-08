82843b46cd392b91f359df4d6c92d156da3cfe8c
Jacek <jacekbizub@gmail.com>
2019-05-08 21:23:54 +0200

    Fixed galleries css loading

--- res/js/cssloader.js
+++ res/js/cssloader.js
@@ -2,7 +2,7 @@
     let head  = document.getElementsByTagName('head')[0];
     let link;
     let srcs = ['https://fonts.googleapis.com/css?family=Oswald:200,400,500,700&amp;subset=latin-ext',
-    			'res/css/galleries.css'];
+    			'res/css/galleries.min.css'];
 
     for (src of srcs) {
         link  = document.createElement('link');
