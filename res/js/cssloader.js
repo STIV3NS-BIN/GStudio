50918b305a401564dd8b5fdaa561b32f569bf9b3
Jacek <jacekbizub@gmail.com>
2019-05-08 23:49:17 +0200

    Added photography gallery

--- res/js/cssloader.js
+++ res/js/cssloader.js
@@ -2,7 +2,8 @@
     let head  = document.getElementsByTagName('head')[0];
     let link;
     let srcs = ['https://fonts.googleapis.com/css?family=Oswald:200,400,500,700&amp;subset=latin-ext',
-    			'res/css/galleries.min.css'];
+                'res/css/galleries.min.css',
+                'https://cdn.jsdelivr.net/npm/lightgallery.js@1.1.3/src/css/lightgallery.css'];
 
     for (src of srcs) {
         link  = document.createElement('link');
