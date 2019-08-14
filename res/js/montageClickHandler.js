ebab20f5c2a37898c1a6fae16fb0b0f00e6d4268
STIV3NS <jacekbizub@gmail.com>
2019-08-14 19:20:37 +0200

    Fixed light-gallery

--- res/js/montageClickHandler.js
+++ res/js/montageClickHandler.js
@@ -6,6 +6,8 @@ $('#montage').on('click', (e) => {
         showThumbByDefault: true,
         loadYoutubeThumbnail: true,
         youtubeThumbSize: 'default',
+        videoMaxWidth: '1000px',
+        autoplayFirstVideo: false,
         dynamicEl: [
             {src: 'https://www.youtube.com/watch?v=Z4xTWulHQLM'},
             {src: 'https://www.youtube.com/watch?v=95MKLzSz4QM'},
@@ -13,7 +15,9 @@ $('#montage').on('click', (e) => {
             {src: 'https://www.youtube.com/watch?v=kg43qNTFniI'},
             {src: 'https://www.youtube.com/watch?v=GmPQ8E3R7x8'},
             {src: 'https://www.youtube.com/watch?v=XOVIa3r6gxQ'}
-        ]
+        ],
+        hash: true,
+        galleryId: 2
     });
 
 });
--- res/js/productionClickHandler.js
+++ res/js/productionClickHandler.js
@@ -6,6 +6,8 @@ $('#production').on('click', (e) => {
         showThumbByDefault: true,
         loadYoutubeThumbnail: true,
         youtubeThumbSize: 'default',
+        videoMaxWidth: '1000px',
+        autoplayFirstVideo: false,
         dynamicEl: [
             {src: 'https://www.youtube.com/watch?v=IFlpEhcfbd4'},
             {src: 'https://www.youtube.com/watch?v=hG8DSfz_yDs'},
@@ -13,7 +15,9 @@ $('#production').on('click', (e) => {
             {src: 'https://www.youtube.com/watch?v=NqshGTgA8_c'},
             {src: 'https://www.youtube.com/watch?v=HMTZ4RN3fpg'},
             {src: 'https://www.youtube.com/watch?v=H-wnuJqLlnA'}
-        ]
+        ],
+        hash: true,
+        galleryId: 1
     });
 
 });
