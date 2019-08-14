cfdca01b59a407d3ecfec7d2bf1ebd56acc8c4ff
STIV3NS <jacekbizub@gmail.com>
2019-08-14 20:41:01 +0200

    Encapsulated clickHandlers

--- res/js/montageClickHandler.js
+++ res/js/montageClickHandler.js
@@ -1,23 +1,25 @@
-$('#montage').on('click', (e) => {
-    e.preventDefault();
-    $(this).lightGallery({
-        dynamic: true,
-        thumbnail:true,
-        showThumbByDefault: true,
-        loadYoutubeThumbnail: true,
-        youtubeThumbSize: 'default',
-        videoMaxWidth: '1000px',
-        autoplayFirstVideo: false,
-        dynamicEl: [
-            {src: 'https://www.youtube.com/watch?v=Z4xTWulHQLM'},
-            {src: 'https://www.youtube.com/watch?v=95MKLzSz4QM'},
-            {src: 'https://www.youtube.com/watch?v=nJtna9O4BZA'},
-            {src: 'https://www.youtube.com/watch?v=kg43qNTFniI'},
-            {src: 'https://www.youtube.com/watch?v=GmPQ8E3R7x8'},
-            {src: 'https://www.youtube.com/watch?v=XOVIa3r6gxQ'}
-        ],
-        hash: true,
-        galleryId: 2
+(function($) {
+
+    $('#montage').on('click', (e) => {
+        e.preventDefault();
+        $('#montage').lightGallery({
+            dynamic: true,
+            thumbnail:true,
+            showThumbByDefault: true,
+            loadYoutubeThumbnail: true,
+            youtubeThumbSize: 'default',
+            videoMaxWidth: '1000px',
+            autoplayFirstVideo: false,
+            dynamicEl: [
+                {src: 'https://www.youtube.com/watch?v=Z4xTWulHQLM'},
+                {src: 'https://www.youtube.com/watch?v=95MKLzSz4QM'},
+                {src: 'https://www.youtube.com/watch?v=nJtna9O4BZA'},
+                {src: 'https://www.youtube.com/watch?v=kg43qNTFniI'},
+                {src: 'https://www.youtube.com/watch?v=GmPQ8E3R7x8'},
+                {src: 'https://www.youtube.com/watch?v=XOVIa3r6gxQ'}
+            ]
+        });
+    
     });
 
-});
+})($);
--- res/js/productionClickHandler.js
+++ res/js/productionClickHandler.js
@@ -1,23 +1,25 @@
-$('#production').on('click', (e) => {
-    e.preventDefault();
-    $(this).lightGallery({
-        dynamic: true,
-        thumbnail:true,
-        showThumbByDefault: true,
-        loadYoutubeThumbnail: true,
-        youtubeThumbSize: 'default',
-        videoMaxWidth: '1000px',
-        autoplayFirstVideo: false,
-        dynamicEl: [
-            {src: 'https://www.youtube.com/watch?v=IFlpEhcfbd4'},
-            {src: 'https://www.youtube.com/watch?v=hG8DSfz_yDs'},
-            {src: 'https://www.youtube.com/watch?v=YApLzgQbs7Y'},
-            {src: 'https://www.youtube.com/watch?v=NqshGTgA8_c'},
-            {src: 'https://www.youtube.com/watch?v=HMTZ4RN3fpg'},
-            {src: 'https://www.youtube.com/watch?v=H-wnuJqLlnA'}
-        ],
-        hash: true,
-        galleryId: 1
-    });
+(function($) {
 
-});
+    $('#production').on('click', (e) => {
+        e.preventDefault();
+        $('#production').lightGallery({
+            dynamic: true,
+            thumbnail:true,
+            showThumbByDefault: true,
+            loadYoutubeThumbnail: true,
+            youtubeThumbSize: 'default',
+            videoMaxWidth: '1000px',
+            autoplayFirstVideo: false,
+            dynamicEl: [
+                {src: 'https://www.youtube.com/watch?v=IFlpEhcfbd4'},
+                {src: 'https://www.youtube.com/watch?v=hG8DSfz_yDs'},
+                {src: 'https://www.youtube.com/watch?v=YApLzgQbs7Y'},
+                {src: 'https://www.youtube.com/watch?v=NqshGTgA8_c'},
+                {src: 'https://www.youtube.com/watch?v=HMTZ4RN3fpg'},
+                {src: 'https://www.youtube.com/watch?v=H-wnuJqLlnA'}
+            ]
+        });
+    
+    });
+    
+})($);
