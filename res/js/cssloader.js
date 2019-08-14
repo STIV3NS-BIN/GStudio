9653c08cfd7939b31c58200459cfb6cae6ad231c
STIV3NS <jacekbizub@gmail.com>
2019-08-14 22:27:38 +0200

    Added contact section

--- res/js/cssloader.js
+++ res/js/cssloader.js
@@ -2,6 +2,7 @@
     let head  = document.getElementsByTagName('head')[0];
     let link;
     let srcs = ['https://fonts.googleapis.com/css?family=Oswald:200,400,500,700&amp;subset=latin-ext',
+                'res/css/fontello.css',
                 'res/css/galleries.min.css',
                 'https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.6.11/css/lightgallery.min.css'];
 
