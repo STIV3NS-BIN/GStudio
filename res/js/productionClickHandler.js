15f9fe42707aa144ae863de725f06ac080570969
STIV3NS <jacekbizub@gmail.com>
2019-09-23 18:55:50 +0200

    Update productionClickHandler.js

--- res/js/productionClickHandler.js
+++ res/js/productionClickHandler.js
@@ -17,8 +17,7 @@
                 {src: 'https://www.youtube.com/watch?v=NqshGTgA8_c'},
                 {src: 'https://www.youtube.com/watch?v=HMTZ4RN3fpg'},
                 {src: 'https://www.youtube.com/watch?v=b7aOw_dLbRk'},
-                {src: 'https://www.youtube.com/watch?v=H-wnuJqLlnA'},
-                {src: 'https://www.facebook.com/Gonitwa.Podhalanska/videos/889143941458463/?__xts__[0]=68.ARCvJ-euLbPQSmtldFM-SLDx59dmxMtvMRlZpX7SrAUhxb-UVr3C9HRYQ7FR1tJ_qK8Mcwpj9ggrshmnq8JzE8l6N6d-6Q_TtnBr6tWSbfrulYLIfTRKFm1vcAqRDUeHCnfel-wZiRuRj1QIjp_pZ_90Tmz5elVpIgcNrONceiWIWy7sKtiK4E2ojGZ7m3jYyeaLLYzo3Lwwc9QT9Ps-SZ7ucp430w6QmZOrurUL1gjGqNZzxi4UY1wfLuz1cMAxoCHWdQ7nfpDfsLPEg-bhmAn-jRDAFGvhd9wG1iMx2s3tTos0hHtuqZd9DiiBhTaZX6Q4xx_zw2CPwtOLTqJdHdl47_JWbZDn8llHkQ&__tn__=-R'}
+                {src: 'https://www.youtube.com/watch?v=H-wnuJqLlnA'}
             ]
         });
     
