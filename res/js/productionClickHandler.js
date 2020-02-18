738f9fa9b39aa313ead44111b80d85e93c4c0cc0
gstudio4 <55462479+gstudio4@users.noreply.github.com>
2020-02-18 23:33:56 +0100

    Update productionClickHandler.js

--- res/js/productionClickHandler.js
+++ res/js/productionClickHandler.js
@@ -11,6 +11,7 @@
             videoMaxWidth: '1000px',
             autoplayFirstVideo: false,
             dynamicEl: [
+                {src: 'https://youtu.be/lrDmmM5u-4o'},
                 {src: 'https://youtu.be/rsYM7AbjkAw'},
                 {src: 'https://www.youtube.com/watch?v=hG8DSfz_yDs'},
                 {src: 'https://www.youtube.com/watch?v=YApLzgQbs7Y'},
