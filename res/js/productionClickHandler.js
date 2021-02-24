ed8b69f56dfc7b336adcba78ce8f68428cec542d
gstudio4 <55462479+gstudio4@users.noreply.github.com>
2021-02-24 15:10:17 +0100

    Update productionClickHandler.js

--- res/js/productionClickHandler.js
+++ res/js/productionClickHandler.js
@@ -13,16 +13,16 @@
             dynamicEl: [
                 {src: 'https://youtu.be/t4hUfyKmo3k'},
                 {src: 'https://youtu.be/xZeXYhCHYTU'},
-                {src: 'https://youtu.be/R6mmLR8GO1E'},
                 {src: 'https://youtu.be/ipm4mT3XNkc'},
-                {src: 'https://youtu.be/lrDmmM5u-4o'},
-                {src: 'https://youtu.be/rsYM7AbjkAw'},
-                {src: 'https://www.youtube.com/watch?v=hG8DSfz_yDs'},
-                {src: 'https://www.youtube.com/watch?v=YApLzgQbs7Y'},
-                {src: 'https://www.youtube.com/watch?v=NqshGTgA8_c'},
-                {src: 'https://www.youtube.com/watch?v=H-wnuJqLlnA'},
                 {src: 'https://www.youtube.com/watch?v=b7aOw_dLbRk'},
                 {src: 'https://www.youtube.com/watch?v=IFlpEhcfbd4'},
+                {src: 'https://www.youtube.com/watch?v=NqshGTgA8_c'},                
+                {src: 'https://youtu.be/lrDmmM5u-4o'},
+                {src: 'https://youtu.be/rsYM7AbjkAw'},
+                {src: 'https://youtu.be/R6mmLR8GO1E'},
+                {src: 'https://www.youtube.com/watch?v=hG8DSfz_yDs'},
+                {src: 'https://www.youtube.com/watch?v=YApLzgQbs7Y'},
+                {src: 'https://www.youtube.com/watch?v=H-wnuJqLlnA'},
                 {src: 'https://www.youtube.com/watch?v=_TvUWn9WOtA'}
             ]
         });
